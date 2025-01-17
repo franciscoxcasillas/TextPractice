250c961ea8a04e623540cf6964eddf9e4aab0ff0
Francisco Casillas <chiccasillas@gmail.com>
2025-01-17 12:48:12 -0600

    Initial Commit

---
+++ Text/TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextApp.swift
+//  Text
+//
+//  Created by Francisco Jean on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
