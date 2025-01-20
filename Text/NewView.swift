7497ab9376fed9a0673d6e0491382825daf71020
Francisco Casillas <chiccasillas@gmail.com>
2025-01-20 14:16:39 -0600

    Second commit to try

---
+++ Text/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  Text
+//
+//  Created by Francisco Jean on 20/01/25.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text("Hello, Max!")
+    }
+}
+
+#Preview {
+    NewView()
+}
